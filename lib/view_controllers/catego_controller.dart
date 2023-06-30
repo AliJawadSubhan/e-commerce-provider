a84b06bffdf18502b2824823696768661a4889a8
Ali Jawad <jawadsubhan7@gmail.com>
2023-06-30 13:11:22 +0500

    home ui

---
+++ lib/view_controllers/catego_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:multivendorapp/view_controllers/home_controller.dart';
+
+class CategoriesController extends ChangeNotifier {
+  late HomeController hcontroller = HomeController();
+}
